4343937e1a1b21fbaf20a3911e227131f14b03f6
Gareth G. Aye <gareth.aye@gmail.com>
2012-02-24 14:46:22 -0500

    Adding box intersect impl

--- ray/surface/Box.java
+++ ray/surface/Box.java
@@ -28,8 +28,36 @@ public class Box extends Surface {
 	public boolean intersect(IntersectionRecord outRecord, Ray rayIn) {
 		// TODO: fill in this function.
 		// You will need to implement the three-slab intersection test
+		double txMin = (minPt.x - rayIn.origin.x) / rayIn.direction.x;
+		double txMax = (maxPt.x - rayIn.origin.x) / rayIn.direction.x;
+		if (txMin > txMax) {
+			swap(txMin, txMax);
+		}
 		
-		return false;
+		double tyMin = (minPt.y - rayIn.origin.y) / rayIn.direction.y;
+		double tyMax = (maxPt.y - rayIn.origin.y) / rayIn.direction.y;
+		if (tyMin > tyMax) {
+			swap(tyMin, tyMax);
+		}
+		
+		double tzMin = (minPt.z - rayIn.origin.z) / rayIn.direction.z;
+		double tzMax = (maxPt.z - rayIn.origin.z) / rayIn.direction.z;
+		if (tzMin > tzMax) {
+			swap(tzMin, tzMax);
+		}
+		
+		double mint = Math.max(txMin, Math.max(tyMin, tzMin));
+		double maxt = Math.min(txMax, Math.min(tyMax, tzMax));
+		if (mint > maxt) {
+			return false;
+		}
+		
+		outRecord.t = mint;
+		rayIn.end = outRecord.t;
+		outRecord.surface = this;
+		Vector3 scaledDirection = Vector3.getScaledVector(rayIn.direction, outRecord.t);
+		outRecord.location.add(rayIn.origin, scaledDirection);
+		return true;
 	}
 	
 	/**
@@ -37,6 +65,12 @@ public class Box extends Surface {
 	 */
 	public String toString() {
 		return "Box " + minPt + " " + maxPt + " " + shader + " end";
+	}
+	
+	private void swap(double a, double b) {
+		double tmp = a;
+		a = b;
+		b = tmp;
 	}
 }
 
